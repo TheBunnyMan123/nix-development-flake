3f12d4260653c399fe79f0371a3c9131fa393a2d
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-05-05 01:10:30 -0500

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+This flake will automatically open vs code with various java extensions
+
+`nix develop github:TheBunnyMan123/java-development-flake`
---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A dev environment for Figura";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        system = "${system}";
+      };
+    in {
+      devShell = pkgs.mkShell {
+        packages = with pkgs; [
+          bashInteractive
+          (vscode-with-extensions.override {
+            vscode = vscodium;
+            vscodeExtensions = with vscode-extensions; [
+              bbenoist.nix
+            ];
+          })
+        ];
+
+        shellHook = ''
+          exec codium .
+        '';
+      };
+    }
+  );
+}
